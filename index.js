32be032018c06db3101c3845f9cea4236e07a7ac
Joseph Lorenzo <josephclorenzo@gmail.com>
2018-10-20 17:37:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,36 @@
+//FIRST FUNCTION takeANumber
+var deliCounter = [];
+
+function takeANumber(deliCounter, name) {
+	deliCounter.push(name);
+	return "Welcome, " + name + ". You are number " + deliCounter.length + " in line.";  
+}
+
+console.log(takeANumber(deliCounter, "Ada"));
+console.log(takeANumber(deliCounter, "Joe"));
+
+//SECOND FUNCTION nowServing
+function nowServing(deliCounter) {
+	if (deliCounter.length === 0) {
+		return "There is nobody waiting to be served!";	
+	}
+	else {
+		return "Currently serving " + deliCounter.shift() + ".";
+	}
+}
+
+//THIRD FUNCTION currentLine
+function currentLine(deliCounter) {
+	var str = "The line is Currently: "
+	if(deliCounter.length !== 0) {
+		for(var i = 0; i < deliCounter.length; i++) {
+			str = str + (i + 1) + ". " + deliCounter[i] + ", ";
+		}
+		return str;
+	}
+	else if (deliCounter.length === 0){
+		return "The line is currently empty."
+	}
+}
+deliCounter.length = 0;
+console.log(currentLine(deliCounter));
